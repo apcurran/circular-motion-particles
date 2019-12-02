5bdc04e8c9f02b78fd69dacc8e125e68d55d1a07
apc <alexcurran.va@gmail.com>
2019-12-02 17:27:50 -0600

    Added randomized color from array

--- script.js
+++ script.js
@@ -7,12 +7,20 @@ const animation = (() => {
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
 
+    const colors = [
+        "#8B09FF",
+        "#6A12E8",
+        "#4A40FF",
+        "#5009FF"
+    ]
+
     class Particle {
-        constructor(moveRadius, step, pos, size) {
+        constructor(moveRadius, step, pos, size, color) {
             this.moveRadius = moveRadius;
             this.step = step;
             this.pos = pos;
             this.size = size;
+            this.color = color;
         }
 
         draw() {
@@ -20,9 +28,9 @@ const animation = (() => {
             const y = Math.sin(this.pos) * this.moveRadius + canvas.height / 2;
             ctx.beginPath();
             ctx.arc(x, y, this.size, 0, Math.PI * 2);
-            ctx.fillStyle = "#fff";
-            ctx.fill();
             ctx.closePath();
+            ctx.fillStyle = this.color;
+            ctx.fill();
         }
 
         update() {
@@ -32,21 +40,25 @@ const animation = (() => {
     }
 
     let particleArr = [];
+
     function init() {
         particleArr = [];
-        for (let i = 0; i < 500; i++) {
-            let moveRadius = Math.floor(Math.random() * canvas.width + 25);
-            let step = (Math.random() * 0.002) + 0.002;
-            let pos = Math.floor(Math.random() * (Math.PI * 2));
-            let size = Math.floor(Math.random() * 8 + 0.5);
+        const totalParticles = Math.floor(canvas.width / 4);
 
-            particleArr.push(new Particle(moveRadius, step, pos, size));
+        for (let i = 0; i < totalParticles; i++) {
+            let moveRadius = Math.floor(Math.random() * canvas.width + 50);
+            let step = Math.random() * 0.002 + 0.002;
+            let pos = Math.random() * (Math.PI * 2);
+            let size = Math.floor(Math.random() * 6 + 0.5);
+            let color = colors[Math.floor(Math.random() * colors.length)];
+
+            particleArr.push(new Particle(moveRadius, step, pos, size, color));
         }
     }
 
     function animate() {
         requestAnimationFrame(animate);
-        ctx.fillStyle = "rgba(0, 0, 0, .1)";
+        ctx.fillStyle = "rgba(255, 255, 255, .04)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         for (const particle of particleArr) {
